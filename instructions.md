f57a75530959d9146f22a42c94b5ead60e7dcd7e
Andre Coetzer <coetzeronline@gmail.com>
2021-04-23 23:44:00 +0300

    Challenge added

---
+++ instructions.md
@@ -0,0 +1,3 @@
+Exercise 1: 
+Given two integer numbers return their product. 
+If the product is greater than 1000, then return their sum.
---
+++ python_beginner_one.py
@@ -0,0 +1,14 @@
+# Given 1
+# number1 = 20
+# number2 = 30
+
+# Place your code beneath this
+# expected result is 600
+
+
+# Given 2
+# number1 = 40
+# number2 = 30
+
+# Place your code beneath this
+# expected result is 70
